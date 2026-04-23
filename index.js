878a82edc0ee0c15fae6a926eb955f805c151f5d
robertoroliveira <roberto.inesquesivel@gmail.com>
2026-04-23 19:44:14 -0300

    fix duplicate Pool

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 // 🔌 CONEXÃO COM POSTGRESQL
-const { Pool } = require("pg");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
