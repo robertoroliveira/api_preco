97cc97d159c42a72f180a892a8c11564330bdf51
robertoroliveira <roberto.inesquesivel@gmail.com>
2026-04-23 23:52:46 -0300

    update API

--- index.js
+++ index.js
@@ -20,7 +20,30 @@ app.get("/produto/:codigo", async (req, res) => {
 
   try {
     const result = await pool.query(
-      "SELECT * FROM produtos p join vendas v on v.produto_id = p.id WHERE codigo = $1",
+      "SELECT 
+  
+  p.codigo,
+  p.nome,
+  p.preco as preco_venda,
+  c.preco_compra,
+  c.data_compra::DATE,
+  COALESCE(c.total_comprado, 0) AS total_comprado,
+  COALESCE(v.total_vendido, 0) AS total_vendido,
+COALESCE(c.total_comprado, 0) - COALESCE(v.total_vendido, 0) AS Estoque_Atual
+
+FROM produtos p
+
+LEFT JOIN (
+  SELECT produto_id, preco_compra, data_compra, SUM(qtd_compra) AS total_comprado
+  FROM compras
+  GROUP BY produto_id, preco_compra, data_compra
+) c ON c.produto_id = p.id
+
+LEFT JOIN (
+  SELECT produto_id, SUM(quantidade) AS total_vendido
+  FROM vendas
+  GROUP BY produto_id
+) v ON v.produto_id = p.id WHERE codigo = $1",
       [codigo]
     );
 
